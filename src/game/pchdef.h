7a9f539275c9582389cb96afa18022794ffdc0d6
VladimirMangos <Vladimir@cmangos.net>
2013-06-05 16:17:04 +0400

    Update pchdef.h
    Removed ACE includes, added used boost headers

--- src/game/pchdef.h
+++ src/game/pchdef.h
@@ -1,19 +1,23 @@
 // add here most rarely modified headers to speed up debug build compilation
-#include <ace/Basic_Types.h>
-#include <ace/Synch_Traits.h>
-#include <ace/Svc_Handler.h>
-#include <ace/SOCK_Stream.h>
-#include <ace/SOCK_Acceptor.h>
-#include <ace/Acceptor.h>
-#include <ace/Unbounded_Queue.h>
-#include <ace/Message_Block.h>
-
 #include <boost/config.hpp>
 #include <boost/version.hpp>
 #include <boost/asio.hpp>
+#include <boost/atomic.hpp>
+#include <boost/bind.hpp>
+#include <boost/cstdint.hpp>
 #include <boost/date_time.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/program_options.hpp>
+#include <boost/scoped_array.hpp>
 #include <boost/thread.hpp>
+#include <boost/thread/lock_guard.hpp>
 #include <boost/thread/mutex.hpp>
+#include <boost/thread/recursive_mutex.hpp>
+#include <boost/thread/shared_mutex.hpp>
+#include <boost/thread/shared_lock_guard.hpp>
+#include <boost/thread/tss.hpp>
 
 #include "Common.h"
 
